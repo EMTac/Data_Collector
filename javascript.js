4f7db1cc6566d67ad662d88b4a156edb35f449d7
EMTac <115601690+EMTac@users.noreply.github.com>
2023-02-21 13:42:55 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([47.258728, -122.465973], 13);
+var map = L.map('map').setView([47.5, -122.3], 9);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
